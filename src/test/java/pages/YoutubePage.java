981d66030f793cb5852f4c858697151e73ab67fa
xXFranzFXx <franz.fernando@testpro.io>
2024-03-22 13:45:32 -0700

    Adds testcase

--- src/test/java/pages/YoutubePage.java
+++ src/test/java/pages/YoutubePage.java
@@ -8,24 +8,32 @@ import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
 
 import java.time.Duration;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class YoutubePage extends BasePage{
     @CacheLookup
-    @FindBy(css = "#expand .tp-yt-paper-button #waves")
+    @FindBy(css = "#expand")
     private WebElement moreLocator;
+    @FindBy(css = "ytd-transcript-segment-renderer[rounded-container] .segment-timestamp.ytd-transcript-segment-renderer")
+    private List<WebElement> transcrSegTimeStamp;
+    @FindBy(css = ".segment.ytd-transcript-segment-renderer .segment-text.ytd-transcript-segment-renderer")
+    private List<WebElement> transcrSegText;
     @CacheLookup
     @FindBy(css = "#primary #primary-button .yt-spec-touch-feedback-shape")
     private WebElement transcriptButton;
     @CacheLookup
     @FindBy(css="#subscribe-button-shape .yt-spec-touch-feedback-shape__fill")
     private WebElement subscribeButton;
-    private By subscribeBtn = By.cssSelector("#subscribe-button-shape .yt-spec-touch-feedback-shape__fill");
+    private By subscribeBtn = By.cssSelector("#subscribe-button-shape button");
     public YoutubePage(WebDriver givenDriver) {
         super(givenDriver);
     }
 
     public boolean isSubscribeBtnVisible() {
-        actions.pause(30).perform();
+        actions.pause(40).perform();
         return find(subscribeBtn).isDisplayed();
     }
     public YoutubePage clickMore() {
@@ -41,6 +49,20 @@ public class YoutubePage extends BasePage{
         actions.pause(Duration.ofSeconds(7)).perform();
         return this;
     }
+
+    public List<String> getTimeStamps() {
+       return transcrSegTimeStamp.stream().map(WebElement::getText).toList();
+    }
+    public List<String> getSegmentText() {
+     return transcrSegText.stream().map(WebElement::getText).toList();
+    }
+    public Map<String, String> createMap() {
+        return IntStream.range(0, getTimeStamps().size())
+                .boxed()
+                .collect(Collectors.toMap(
+                        getTimeStamps()::get, getSegmentText()::get));
+    }
+
 
 
 }
--- src/test/java/tests/YoutubePageTests.java
+++ src/test/java/tests/YoutubePageTests.java
@@ -5,23 +5,22 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
-import pages.BasePage;
-import pages.EmpathTraumaPage;
-import pages.YoutubePage;
 
-import java.io.IOException;
-import java.util.List;
+import pages.YoutubePage;
 
 public class YoutubePageTests extends BaseTest {
     YoutubePage youtubePage;
+
     @BeforeMethod
     @Parameters({"youtubeURL"})
     public void setupBrowser(String youtubeURL) {
         getDriver().get(youtubeURL);
     }
+
     @Test(description = "Get transcript text from every audio excerpt and store it in a List")
-    public void clickTranscriptButton(){
+    public void clickTranscriptButton() {
         youtubePage = new YoutubePage(getDriver());
         youtubePage.clickMore().clickTranscript();
+        Assert.assertNotEquals(youtubePage.getTimeStamps(), youtubePage.createMap().keySet());
     }
 }
