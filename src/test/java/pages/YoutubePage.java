d9096d294f02976138e7e9ee76a41a4e7e770618
xXFranzFXx <franz.fernando@testpro.io>
2024-03-23 10:28:54 -0700

    adds youtube transcription tests

--- src/test/java/pages/YoutubePage.java
+++ src/test/java/pages/YoutubePage.java
@@ -6,17 +6,23 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class YoutubePage extends BasePage{
     @CacheLookup
     @FindBy(css = "#expand")
     private WebElement moreLocator;
+    @FindBy(css = "#label-text")
+    private WebElement transcriptLang;
+    @FindBy(css = "#title > h1 > yt-formatted-string")
+    private WebElement title;
     @FindBy(css = "ytd-transcript-segment-renderer[rounded-container] .segment-timestamp.ytd-transcript-segment-renderer")
     private List<WebElement> transcrSegTimeStamp;
     @FindBy(css = ".segment.ytd-transcript-segment-renderer .segment-text.ytd-transcript-segment-renderer")
@@ -33,7 +39,7 @@ public class YoutubePage extends BasePage{
     }
 
     public boolean isSubscribeBtnVisible() {
-        actions.pause(40).perform();
+        WebElement elementWait = new WebDriverWait(driver, Duration.ofSeconds(35)).until(ExpectedConditions.presenceOfElementLocated(subscribeBtn));
         return find(subscribeBtn).isDisplayed();
     }
     public YoutubePage clickMore() {
@@ -46,12 +52,12 @@ public class YoutubePage extends BasePage{
     public YoutubePage clickTranscript() {
         actions.moveToElement(transcriptButton).perform();
         clickElement(transcriptButton);
-        actions.pause(Duration.ofSeconds(7)).perform();
         return this;
     }
 
     public List<String> getTimeStamps() {
-       return transcrSegTimeStamp.stream().map(WebElement::getText).toList();
+        List<WebElement> timeStamps = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.cssSelector("ytd-transcript-segment-renderer[rounded-container] .segment-timestamp.ytd-transcript-segment-renderer")));
+        return timeStamps.stream().map(WebElement::getText).toList();
     }
     public List<String> getSegmentText() {
      return transcrSegText.stream().map(WebElement::getText).toList();
@@ -62,7 +68,20 @@ public class YoutubePage extends BasePage{
                 .collect(Collectors.toMap(
                         getTimeStamps()::get, getSegmentText()::get));
     }
+    public List<String> mapToList() {
+        Map<String, String> newMap = new HashMap<>(createMap());
+        Set<Map.Entry<String, String>> entries = newMap.entrySet();
+        List<String> transcriptList = new ArrayList<>(entries.stream().map(entry -> entry.getKey() + " " + entry.getValue()).sorted().toList());
+        transcriptList.addFirst(getTranscriptLanguage());
+        transcriptList.addFirst("video url: " + driver.getCurrentUrl());
+        transcriptList.addFirst(getTitle());
+        return transcriptList;
+    }
 
-
-
+    public String getTitle() {
+        return findElement(title).getText();
+    }
+    public String getTranscriptLanguage() {
+        return findElement(transcriptLang).getText();
+    }
 }
--- src/test/java/tests/YoutubePageTests.java
+++ src/test/java/tests/YoutubePageTests.java
@@ -2,11 +2,19 @@ package tests;
 
 import base.BaseTest;
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import pages.YoutubePage;
+import util.TranscriptUtil;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class YoutubePageTests extends BaseTest {
     YoutubePage youtubePage;
@@ -18,9 +26,11 @@ public class YoutubePageTests extends BaseTest {
     }
 
     @Test(description = "Get transcript text from every audio excerpt and store it in a List")
-    public void clickTranscriptButton() {
+    public void clickTranscriptButton() throws IOException {
         youtubePage = new YoutubePage(getDriver());
         youtubePage.clickMore().clickTranscript();
-        Assert.assertNotEquals(youtubePage.getTimeStamps(), youtubePage.createMap().keySet());
+        List<String> transcriptList =  youtubePage.mapToList();
+        TranscriptUtil.convertTranscriptToFile(transcriptList, "youtubeTranscript");
+        Assert.assertEquals(youtubePage.createMap().keySet().size(), youtubePage.getTimeStamps().size());
     }
 }
