126550cee667beab09138d7987cac71a40be1f54
xXFranzFXx <franz.fernando@testpro.io>
2024-04-26 14:10:39 -0700

    syntax corrections

--- src/test/java/tests/EasyYesLeadsTests.java
+++ src/test/java/tests/EasyYesLeadsTests.java
@@ -1,34 +1,25 @@
 package tests;
 
 import base.BaseTest;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.devtools.DevTools;
 import org.openqa.selenium.devtools.HasDevTools;
 
 import org.openqa.selenium.devtools.v121.network.Network;
 import org.openqa.selenium.devtools.v121.network.model.Request;
-import org.openqa.selenium.devtools.v121.network.model.Response;
 import org.testng.Assert;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import pages.EasyYesLeadsPage;
-import util.TestUtil;
 import util.TranscriptUtil;
 
-import java.io.File;
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.SocketTimeoutException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
-import java.util.stream.Collectors;
 
 import static util.TranscriptUtil.convertTranscriptToFileFromLink;
 
@@ -37,7 +28,6 @@ public class EasyYesLeadsTests extends BaseTest {
     private final String fileName = "audioLinks";
     private final int pause = 12000;
     private final int expectedListSize = 6;
-
 
     @Test(description = "Use selenium devtools to capture network request from click event, then write the captured request data to a txt file")
     @Parameters({"easyYesURL"})
@@ -67,7 +57,6 @@ public class EasyYesLeadsTests extends BaseTest {
         Assert.assertEquals(audioLinks.size(), expectedListSize);
         devTools.clearListeners();
         devTools.close();
-
     }
     @Test(description = "Read file of links created from previous test to a list, then transcribe each link from that list to a new file")
     public void transcribeLinksFromFile() throws IOException {
