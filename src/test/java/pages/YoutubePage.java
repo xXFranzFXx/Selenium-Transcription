27787a4783bbc8ac032f988236870366cbbc7574
xXFranzFXx <franz.fernando@testpro.io>
2024-05-18 12:53:31 -0700

    Update YoutubePage.java

--- src/test/java/pages/YoutubePage.java
+++ src/test/java/pages/YoutubePage.java
@@ -1,6 +1,5 @@
 package pages;
 
-import org.checkerframework.checker.signature.qual.FqBinaryName;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
