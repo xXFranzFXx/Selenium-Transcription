7b21b233c09ad12544f258b4bec1987f20aea003
xXFranzFXx <franz.fernando@testpro.io>
2024-10-28 04:45:08 -0700

    Update TranscriptUtil.java

--- src/test/java/util/TranscriptUtil.java
+++ src/test/java/util/TranscriptUtil.java
@@ -8,18 +8,17 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
+
 import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
-import java.util.Arrays;
+
 import java.util.List;
-import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
+
 import java.util.stream.Stream;
 
 public class TranscriptUtil {
